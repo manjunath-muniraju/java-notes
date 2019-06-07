ed4c1ee2477da4396b22a5c953b87350ca51c7f3
Manjunath.Muniraju <manjunath.muniraju@ellucian.com>
2019-06-07 17:29:52 +0530

    notes upto HashSet

---
+++ ArrayListDemo.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class ArrayListDemo {
+	public static void main(String args[]) {
+
+		ArrayList al = new ArrayList();
+
+		al.add("A");
+		al.add(10);
+		al.add("A");
+		al.add(null);
+		System.out.println(al);
+		al.remove(2);
+		System.out.println(al);
+		al.add(2,"m");
+		al.add("N");
+		System.out.println(al);
+	}
+}
